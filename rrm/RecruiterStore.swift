e8968fb78589921ff38ad8451eff98ad6a18642b
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-10-31 21:39:27 -0400

    Added O(n) inserting to recruiters array

--- rrm/RecruiterStore.swift
+++ rrm/RecruiterStore.swift
@@ -23,7 +23,8 @@ class RecruiterStore {
     
     @discardableResult func generateRecruiter() -> Recruiter {
         let generatedRecruiter = Recruiter(random: true)
-        recruiters.append(generatedRecruiter)
+        let updatedRecruiters = addNewRecruiterAlphabetically(recruiters: recruiters, newRecruiter: generatedRecruiter)
+        recruiters = updatedRecruiters
         determineSections()
         return generatedRecruiter
     }
@@ -46,15 +47,46 @@ class RecruiterStore {
         sortedLastNameFirstLetters = uniqueLastNameFirstLetters.sorted()
         
         sections = sortedLastNameFirstLetters.map { letter in
-            return recruiters
-                .filter { $0.getFirstLetterOfLastName() == letter }
-                .sorted {
-                    if $0.lastName != $1.lastName {
-                        return $0.lastName < $1.lastName
-                    } else {
-                        return $0.firstName < $1.firstName
+            return recruiters.filter { $0.getFirstLetterOfLastName() == letter }
+        }
+    }
+    
+    func addNewRecruiterAlphabetically(recruiters: [Recruiter], newRecruiter: Recruiter) -> [Recruiter] {
+        var recruiterAdded = false
+        var newList = [Recruiter]()
+        guard recruiters.count > 0 else {
+            newList.append(newRecruiter)
+            return newList
+        }
+        for r in recruiters {
+            if !recruiterAdded {
+                let lastNameRelationshipStatus = getNameRelationshipStatus(oldPerson: r.lastName, newPerson: newRecruiter.lastName)
+                if lastNameRelationshipStatus == .olderNameIsGreater {
+                    newList.append(newRecruiter)
+                    recruiterAdded = true
+                } else if lastNameRelationshipStatus == .equal {
+                    let firstNameRelationshipStatus = getNameRelationshipStatus(oldPerson: r.firstName, newPerson: newRecruiter.firstName)
+                    if firstNameRelationshipStatus == .olderNameIsGreater || firstNameRelationshipStatus == .equal {
+                        newList.append(newRecruiter)
+                        recruiterAdded = true
                     }
+                }
             }
+            newList.append(r)
+        }
+        if !recruiterAdded {
+            newList.append(newRecruiter)
+        }
+        return newList
+    }
+    
+    fileprivate func getNameRelationshipStatus(oldPerson oldName: String, newPerson newName: String) -> NameRelationshipStatus {
+        if oldName == newName {
+            return NameRelationshipStatus.equal
+        } else if oldName > newName {
+            return NameRelationshipStatus.olderNameIsGreater
+        } else {
+            return NameRelationshipStatus.newerNameIsGreater
         }
     }
 }
