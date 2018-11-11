a66873c0165efae1b6c8abff61fe50efe7bd72b5
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-11-11 14:20:51 -0500

    Updating gitignore and adding firebase

--- rrm/AppDelegate.swift
+++ rrm/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
         let dataStore = DataStore()
         
         let tabBarController = window?.rootViewController as! UITabBarController
